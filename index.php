1a1d03e675427a5ec51b177ae89d18abd82c58ed
Fabrice BUREL <burelfabrice35@gmail.com>
2017-10-02 09:35:30 +0200

    suppression des lignes inutiles

--- index.php
+++ index.php
@@ -2,8 +2,5 @@
   $a ="La Vie aquatique (The Life Aquatic with Steve Zissou), film américain sorti en 2004, est le quatrième long-métrage réalisé par Wes Anderson. Il est dédié à l'océanographe français Jacques-Yves Cousteau et s'inspire librement de sa vie sur un ton parodique. Le film est une comédie dramatique qui, sous un aspect fantaisiste, aborde des thèmes forts comme la famille, le deuil, la vengeance, l'angoisse de vieillir ou l'échec.";
   $i = strlen($a);
   $a = str_replace ("e", "", $a,$count);
-  $a = str_replace ("é", "", $a,$count);
-  $a = str_replace ("è", "", $a,$count);
   echo $a;
-
 ?>
